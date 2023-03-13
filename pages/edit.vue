4a0865844cda5b4a11d697af83dcb627bf378673
ngodingbentar <dewaqintoro@gmail.com>
2023-03-13 11:53:00 +0700

    added: edits

--- pages/edit.vue
+++ pages/edit.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="bg-gray-200 text-center py-6">
-    <h3 v-if="!loading">Chat</h3>
+    <h3 v-if="!loading">Edit</h3>
     <h3 v-else>LOADING...</h3>
   </div>
   <div>
@@ -9,74 +9,62 @@
         <div :class="item.role" class="item flex relative mb-4 mx-4">
           <div class="text relative bottom-2 py-1 px-4">
             <p class="w-fit text-left">{{ item.content }}</p>
-            <!-- <div v-html="item.content" class="w-fit text-left"></div> -->
           </div>
         </div>
       </div>
     </div>
   </div>
-  <div class="input">
+  <div class="footer">
     <input v-model="chat" class="chat" />
-    <button @click="getChat" class="ml-4 bg-gray-500 text-white p-3 rounded-lg">Send</button>
+    <button @click="fetchEdit" class="ml-4 bg-gray-500 text-white p-3 rounded-lg">Send</button>
   </div>
 </template>
 
 <script lang="ts" setup>
-// reference: https://platform.openai.com/docs/api-reference/chat/create?lang=node.js
+// reference: https://platform.openai.com/docs/api-reference/edits/create?lang=node.js
 
-import { fetch } from "ofetch";
+import { Configuration, OpenAIApi } from "openai";
 
-const config = useRuntimeConfig()
 interface Message {
   role: string
   content: string
 }
+
+const config = useRuntimeConfig()
+const configuration = new Configuration({
+  apiKey: config.apiKey,
+});
+const openai = new OpenAIApi(configuration);
 
 const content = ref<Message[]>([])
 const chat = ref('')
 const role = ref('user')
 const loading = ref(false)
 
-const fetchChat= async () => {
-  const response = await fetch(
-    "https://api.openai.com/v1/chat/completions",
-    {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-        Authorization: `Bearer ${config.apiKey}`,
-      },
-      body: JSON.stringify({
-        "model": "gpt-3.5-turbo",
-        "messages": [{"role": "user", "content": `${chat.value}`}],
-        "user": `${config.userID}`,
-      }),
-    }
-  )
-  return response.json()
-}
-
-const getChat = async () => {
+const fetchEdit =async () => {
   if (loading.value) return
 
+  loading.value = true
   content.value.push({
     role: role.value,
     content: chat.value
   })
-  loading.value = true
-  
-  await fetchChat()
+
+  await openai.createEdit({
+    model: "text-davinci-edit-001",
+    input: chat.value,
+    instruction: "Fix the spelling mistakes",
+  })
   .then((data) => {
     console.log('data', data)
     content.value.push({
       role: 'ai',
-      content: data?.choices[0]?.message?.content || ''
+      content: data?.data?.choices[0]?.text || ''
     })
     chat.value = ''
     loading.value = false
   })
 }
-
 </script>
 
 <style lang="postcss">
@@ -84,7 +72,7 @@ const getChat = async () => {
   @apply text-white w-fit;
 }
 
-.input {
+.footer {
   @apply px-4 justify-center flex p-4 fixed bottom-0 w-full bg-gray-50;
   .chat {
     @apply w-3/4 bg-gray-300 p-3 rounded-lg;
